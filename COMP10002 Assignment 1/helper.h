23d7203ec474162680fd0a3563636de3348a3356
Sebt <edwinli123456@gmail.com>
2022-09-09 02:05:28 +1000

    test 1 works!
    test 2 and 3... need some work :(

--- COMP10002 Assignment 1/helper.h
+++ COMP10002 Assignment 1/helper.h
@@ -6,61 +6,64 @@
 #ifndef ASSIGNMENT_1_HELPER_H
 #define ASSIGNMENT_1_HELPER_H
 
+#define MAX_WORD_LEN 23
+#define MAX_PARA_LEN 10000
+
 #define PARA_END 1
 #define WORD_FND 2
 
 #define TERM_PUNCT ".,;:!?"
+#define NEEDS_DOTS ",;:"
+#define PARA_SEPARATOR "\n\n"
+#define BBOLD "**"
+#define BBOLD_LEN (2 * strlen(BBOLD))
+#define DDOTS "..."
 #define TERM_CHARS "\n "
 #define BYPASS_PUNCT "-'"
-#define NEEDS_DOTS ",;:"
-#define BOLD "**"
-#define DDOTS "..."
 
-#define MAX_TERMS 50
-#define MAX_WORD_LEN 23
-#define MAX_PARA_LEN 10000
 #define MAX_SNIPPET_LEN 30
 #define MIN_SNIPPET_LEN 20
 #define MAX_OUTPUT_LINE 72
 
+#define MAX_TERMS 50
+#define NO_MATCH (-1)
+
 // Constant Types
 typedef char String[MAX_WORD_LEN + 1];    // char[24]
-typedef String StringList[MAX_PARA_LEN];  // char[10000][24]
+typedef String Strings[MAX_PARA_LEN];  // char[10000][24]
 
 // Helper functions
-
 int max(int n1, int n2) { return n1 > n2 ? n1 : n2; }
 int min(int n1, int n2) { return n1 < n2 ? n1 : n2; }
-
-size_t getLastIndex(char *str) { return strlen(str) - 1; }
-
-char getLast(char *str) { return str[getLastIndex(str)]; }
-
+size_t getLastIndex(const char *str) { return strlen(str) - 1; }
 void removeChar(char *str, int i) { strcpy(&str[i], "\0"); }
-
-void clear(void *arr_ptr, size_t size) { memset(arr_ptr, 0, size); }
-
-int isPunctuated(char c) { return strchr(TERM_PUNCT, c) != NULL; }
+void clear(void *arr_ptr, const size_t size) { memset(arr_ptr, 0, size); }
+int isPunctuated(const char c) { return strchr(TERM_PUNCT, c) != NULL; }
 
 /*
  * Returns the index of the punctuation, or 0 if not found.
  */
-int getPunctIndex(char *c) {
+int getPunctIndex(const char *c) {
     for (size_t i = getLastIndex(c); i > 0; i--)
         if (strchr(TERM_PUNCT, c[i]) != NULL) return i;
     return 0;
 }
 
-char *toAlpha(const char *str) {
-    int *dup = strdup(str), *dup_ptr = dup, len = 0;
+int alpha_len(const char *c) {
+    int i = 0;
+    for (; c[i]; i++)
+        if (c[i] == EOF || !isalpha(c[i])) break;
+    return i;
+}
 
-    while (*dup_ptr) {
-        if (isalnum(*dup_ptr)) dup[len++] = *dup_ptr;
-        dup_ptr++;
+int need_dots(const char *c) {
+    for (size_t i = getLastIndex(c); i > 0; i--) {
+        if (strchr(NEEDS_DOTS, c[i]) != NULL) return 1;
+        if (!isalpha(c[i]) && !strchr(BBOLD, c[i])) return 0;
+        if (strchr(TERM_CHARS, c[i])) continue;
+        break;
     }
-
-    dup[len] = '\0';
-    return dup;
+    return 1;
 }
 
 char *getLower(const char *str) {
@@ -70,7 +73,7 @@ char *getLower(const char *str) {
     return dup;
 }
 
-int isQuery(char *str, char *argv[]) {
+int isQuery(const char *str, const char *argv[]) {
     for (int j = 0; argv[j]; ++j)
         if (strstr(getLower(str), getLower(argv[j]))) return j;
     return -1;
@@ -78,21 +81,22 @@ int isQuery(char *str, char *argv[]) {
 
 int get_word(char *word, const int limit) {
     int chr = 0, len = 0;
-    while (chr != EOF && !isalnum(chr = getchar()));
+    while (chr != EOF && !isalnum(chr = getchar()))
+        ;
     if (chr == EOF) return EOF;
 
     while (limit > len && (word[len++] = chr) && (chr = getchar())) {
         if (isalnum(chr) || isPunctuated(chr))
-            continue;  // Store alnum & term puncuations.
+            continue;  // Store alpha numeric & term punctuations.
         if (strchr(BYPASS_PUNCT, chr))
-            chr = ' ';  // Replace bypass puncuation with space
+            chr = ' ';  // Replace bypass punctuation with space
         while (!strchr(TERM_CHARS, (chr)))
             chr = getchar();  // Ignore chars until space or new line.
 
-        // Check for PARA_END
-        if ((chr = getchar()) == '\n') {
+        // Check for PARA_END or EOF
+        if ((chr = getchar()) && chr == EOF || chr == '\n') {
             word[len] = '\0';
-            return PARA_END;
+            return chr == EOF ? EOF : PARA_END;
         } else ungetc(chr, stdin);
         break;
     }
--- COMP10002 Assignment 1/main.c
+++ COMP10002 Assignment 1/main.c
@@ -1,120 +1,140 @@
 #include <stdio.h>
 #include <string.h>
+
 #include "helper.h"
 
 // Constant Values
+#define MAX_WORD_LEN 23
+#define MAX_PARA_LEN 10000
+
 #define PARA_END 1
 #define WORD_FND 2
 
 #define TERM_PUNCT ".,;:!?"
+#define NEEDS_DOTS ",;:"
+#define PARA_SEPARATOR "\n\n"
+#define BBOLD "**"
+#define DDOTS "..."
 #define TERM_CHARS "\n "
 #define BYPASS_PUNCT "-'"
-#define NEEDS_DOTS ",;:"
-#define BOLD "**"
-#define DDOTS "..."
 
-#define MAX_TERMS 50
-#define MAX_WORD_LEN 23
-#define MAX_PARA_LEN 10000
 #define MAX_SNIPPET_LEN 30
 #define MIN_SNIPPET_LEN 20
 #define MAX_OUTPUT_LINE 72
 
-
+#define MAX_TERMS 50
+#define NO_MATCH (-1)
 
 /*
- * Start - inclusive
+ * Start - Inclusive
+ * End - Exclusive
  */
-double getScore(StringList words, int start, int end, char *argv[]) {
+double getScore(const Strings words, int start, int end, const char *argv[]) {
     int key_indexes[MAX_TERMS], last_key_index = 0, key_index;
     double rule1 = 0, rule2 = 0, rule3 = 0, rule4 = 0, rule5 = 0, rule6 = 0;
+    clear(key_indexes, MAX_TERMS);
 
     // ---- 1. Add 15 / (start + 10) points. First word = 0; ----
     rule1 += 15.0 / (start + 10.0);
 
-    for (int i = start; i < end; ++i) {
-        if((key_index = isQuery(words[i], argv)) == -1) continue;
+    for (int i = start; i < end; i++) {
+        if ((key_index = isQuery(words[i], argv)) == -1) continue;
 
         // ---- 3. Add 1.0 points for every other repeated query term ----
-        for (int j = 0; argv[j]; j++) if(key_indexes[j] == key_index && (rule3++)) break;
+        for (int j = 0; argv[j]; j++) {
+            if (key_indexes[j] == key_index) {
+                rule3++;
+                break;
+            }
+        }
 
-        // ---- 2. Add len(query) / 2 points for each unique query term ----
-        if(rule3 == 0 && (rule2 += strlen(words[i]) / 2)) key_indexes[last_key_index++] = key_index;
+        // ---- 2. Add alpha_len(query) / 2 points for each unique query term ----
+        if (rule3 == 0) {
+            rule2 += (double)alpha_len(words[i]) / 2;
+            key_indexes[last_key_index++] = key_index;
+        }
     }
 
-    // ---- 4. Add 0.6 points if the word before is punctuated. (First word = punctuated) ----
-    if(start == 0 || getPunctIndex(words[start - 1])) rule4 += 0.6;
+    // ---- 4. Add 0.6 points if the word before is punctuated. ----
+    if (start == 0 || getPunctIndex(words[start - 1])) rule4 += 0.6;
 
     // ---- 5. Add 0.3 points if the ending word is punctuated. ----
-    if(getPunctIndex(words[end - 1])) rule5 += 0.3;
+    if (getPunctIndex(words[end - 1])) rule5 += 0.3;
 
     // ---- 6. Subtract 0.1 point for each word over MIN_SNIPPET_LEN ----
     rule6 += max((end - start) - MIN_SNIPPET_LEN, 0) * -0.1;
 
-    printf("[1]: %lf, [2]: %lf, [3]: %lf, [4]: %lf, [5]: %lf, [6]: %lf\n", rule1, rule2, rule3, rule4, rule5, rule6);
+    double score = rule1 + rule2 + rule3 + rule4 + rule5 + rule6;
 
-    return rule1 + rule2 + rule3 + rule4 + rule5 + rule6;
-}
-
-double getLowestScore(StringList words, int len, char *argv[], int *begin, int *finish) {
-    if (len <= MIN_SNIPPET_LEN) return getScore(words, 0, len, argv);
-    double score = 0, tmp_score;
-
-    for (int start = 0; start < len; start++) {
-        for (int end = min(len, start + MIN_SNIPPET_LEN); (end - start) <= MAX_SNIPPET_LEN; end++) {
-            printf("\nStart: %d, End: %d\n", start, end);
-            if((tmp_score = getScore(words, start, end, argv)) > score) {
-                score = tmp_score;
-                *begin = start;
-                *finish = end;
-                printf("Start: %d, End: %d, Score: %lf\n", *begin, *finish, score);
-            }
-        }
-    }
-
+    //    printf("[1]: %.1lf, [2]: %.1lf, [3]: %.1lf, [4]: %.1lf, [5]: %.1lf, [6]:
+    //    %.1lf, [SCORE]: %.1lf\n", rule1, rule2, rule3, rule4, rule5, rule6,
+    //    score);
     return score;
 }
 
-void printPara(const StringList formatWords, int start, int finish) {
-    size_t char_count = 0, len, wrap;
+void getLowestScore(const Strings words, int len, const char *argv[],
+                    int *begin, int *finish, double *score) {
+    double tmp_score;
+    *score = 0;
 
-    for (int i = start; i < finish; i++) {
-        len = strlen(formatWords[i]) + (i != 0 ? 1 : 0);
-        wrap = char_count + len > MAX_OUTPUT_LINE;
-        char_count = wrap ? len - 1 : char_count + len;
+    if (len <= MIN_SNIPPET_LEN + 1) {
+        *begin = 0;
+        *finish = len;
+        *score = getScore(words, 0, len, argv);
+        return;
+    }
 
-        printf("%s%s%s", (i != 0 ? (wrap ? "\n" : " ") : ""), formatWords[i],
-               (i == finish - 1 ? "\n\n" : ""));
+    for (int start = 0; start < len - MAX_SNIPPET_LEN; start++) {
+        for (int end = min(len, start + MIN_SNIPPET_LEN);
+             end - start <= MAX_SNIPPET_LEN; end++) {
+            if ((tmp_score = getScore(words, start, end, argv)) > *score) {
+                *score = tmp_score;
+                *begin = start;
+                *finish = end;
+            }
+        }
     }
 }
 
-void formatPara(const StringList words, StringList output, char *argv[], int *matches) {
-    int punct_i, bold, i = 0;
-    for (*matches = 0; words[i][0]; i++) {
+void printPara(const Strings f_wrds, int start, int finish, int stg_3) {
+    size_t char_count = 0, len, wrap, dots;
+
+    for (int i = start; i < finish; i++) {
+        len = strlen(f_wrds[i]) + (i != 0 ? 1 : 0);
+        wrap = char_count + len > MAX_OUTPUT_LINE;
+        char_count = wrap ? len - 1 : char_count + len;
+        dots = stg_3 && i == finish - 1 && need_dots(f_wrds[i]);
+
+        printf("%s%s%s%s", (i != start ? (wrap ? "\n" : " ") : ""), f_wrds[i],
+               (dots ? DDOTS : ""), (i == finish - 1 ? "\n" : ""));
+    }
+}
+
+void formatPara(const Strings words, Strings output, const char *argv[], int *matches) {
+    int punct, bold;
+
+    for (int i = 0; words[i][0]; i++) {
         String *word = strdup(words[i]), suffix = "", result = "";
         bold = isQuery(*word, argv) != -1;
 
-        // Check for bolding words with punctuation
-        if (bold) {
-            *matches++;
-            strcat(&suffix, BOLD);
-            if(punct_i = getPunctIndex(word), punct_i != 0) {
-                removeChar(*word, punct_i); // Remove punct char
-                strcat(&suffix, &word[i][punct_i]); // Add punct to suffix
+        if (bold && (*matches += 1)) {  // Handles bolding words with punctuation
+            strcat(suffix, BBOLD);
+            if (punct = getPunctIndex(*word), punct != 0) {
+                removeChar(*word, punct);          // Remove punctuation char
+                strcat(suffix, &words[i][punct]);  // Add punctuation to suffix
             }
         }
 
-        snprintf(result, sizeof result,"%s%s%s", (bold ? BOLD : ""), *word, suffix);
+        snprintf(result, sizeof result, "%s%s%s", (bold ? BBOLD : ""), *word, suffix);
         strcpy(output[i], result);
     }
 }
 
-
-
-int main(int argc, char *argv[]) {
+int main(int argc, const char *argv[]) {
     String word;
-    StringList words, formatted_words;
+    Strings words, formatted_words;
     int status = 0, word_count = 0, para_count = 0, matches, start, end;
+    double score;
 
     while (status != EOF) {
         while (status != PARA_END && status != EOF) {
@@ -122,24 +142,24 @@ int main(int argc, char *argv[]) {
             strcpy(words[word_count++], word);
         }
         formatPara(words, formatted_words, argv, &matches);
-        double score = getLowestScore(words, word_count, argv, &start, &end);
+        getLowestScore(words, word_count, argv, &start, &end, &score);
 
-        printf("======= Stage 1 [para %d; %d words]\n", ++para_count, word_count);
+        printf("\n======= Stage 1 [para %d; %d words]\n", ++para_count, word_count);
 
-        printf("\n======= Stage 2 [para %d; %d words; %d matches]\n", para_count, word_count, matches);
-        printPara(formatted_words, 0, word_count);
+        printf("\n======= Stage 2 [para %d; %d words; %d matches]\n", para_count,
+               word_count, matches);
+        printPara(formatted_words, 0, word_count, 0);
 
-        printf("======= Stage 3 [para %d; %d words; Score %.1lf]\n", para_count, word_count, score);
-        printPara(formatted_words, start, end);
+        printf("\n======= Stage 3 [para %d; start %d; length %d; score %.2lf]\n",
+               para_count, start, end - start, score);
+        printPara(formatted_words, start, end, 1);
 
-        status = (status == EOF ? EOF : 0);
-        word_count = 0;
+        status = (status == EOF ? EOF : 0), score = 0, matches = 0, word_count = 0;
+        clear(words, MAX_PARA_LEN);
+        clear(formatted_words, MAX_PARA_LEN);
     }
+
+    printf("\nta daa!\n");
 
     return 0;
 }
-
-
-
-
-
